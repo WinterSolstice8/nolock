1e8eca1b3fabe36dae5c4de755143a796c0c17a6
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2023-05-30 17:16:15 -0700

    Add content

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # nolock
-Ashita v4 addon that allows you to engage/disengage without animation lock
+Ashita v4 addon that allows you to engage/disengage without animation lock, from effects such as spells being cast on you or mob tp moves.
+
+This is seperate from similar plugins/addons like JaZero and Butterfeet in that it does not allow you to slide around during animations.
+Credit to the original ASM patch is unknown to me.
+
+# Known issues
+There may be some slight disconnect to where your weapon is located compared to your hand when engaging/disengaging depending on race & weapon type.
+It is merely cosmetic and wlil fix itself once the animations are complete.
---
+++ nolock.lua
@@ -0,0 +1,25 @@
+addon.author   = 'WinterSolstice8';
+addon.name     = 'nolock';
+addon.version  = '1.0.0';
+
+-- Credit to the original pattern & assembly patch is unknown to me.
+local engagePattern = "66FF81????????66C781????????0807C3"
+local engageAddr = ashita.memory.find("FFXiMain.dll", 0, engagePattern, 0, 0)
+local backupASM = 0
+
+ashita.events.register("load", "load_cb", function()
+	if engageAddr ~= 0 then
+		backupASM = ashita.memory.read_array(engageAddr, 7)
+		
+		-- NOP it out
+		ashita.memory.write_array(engageAddr, {0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90})
+    else
+	    print("[nolock] Could not locate address to patch")
+    end
+end)
+
+ashita.events.register("unload", "unload_cb", function()
+    if engageAddr ~= 0 and backupASM ~= 0 then
+        ashita.memory.write_array(engageAddr, backupASM)
+    end
+end)
